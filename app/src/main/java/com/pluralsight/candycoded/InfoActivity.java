ff47393ba03d9ec3299988d3d0b7038f0e7c757d
Marina Biletska <marina.biletska@gmail.com>
2019-10-27 15:16:07 +0200

    Add intent to call CandyStore number

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -28,7 +28,7 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
-    public void createMapIntent(View view){
+    public void createMapIntent(View view) {
         Uri locationUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, locationUri);
         mapIntent.setPackage("com.google.android.apps.maps");
@@ -41,4 +41,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+        Uri phoneNumUri = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(phoneNumUri);
+        startActivity(phoneIntent);
+    }
 }
